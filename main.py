625e77ec97c28f40b323768cefe3a4ef51dd7b19
Mustafa <musgamal05@gmail.com>
2022-05-05 04:34:47 +0200

    second commit

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ imgBat2 = cv2.imread('Resources/bat2.png', cv2.IMREAD_UNCHANGED)
 # Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 
+# Variables
+ballPos = [100, 100]
+speedX = 10
+speedY = 10
+gameOver = False
+
 while True:
     _, img = cap.read()
     img = cv2.flip(img, 1)
@@ -37,16 +43,41 @@ while True:
             x, y, w, h = hand["bbox"]
             h1, w1, _ = imgBat1.shape
             y1 = y - h1/2
-            print(y1)
+            y1 = np.clip(y1, 20, 415)
 
             if hand['type'] == "Left":
                 img = cvzone.overlayPNG(img, imgBat1, (59,int(y1)))
+                if 59 < ballPos[0] < 59 +w1 and y1< ballPos[1] < y1+h1:
+                    speedX = -speedX
+                    ballPos[0] += 30
+
+            if hand['type'] == "Right":
+                img = cvzone.overlayPNG(img, imgBat2, (1195, int(y1)))
+                if 1195-50 < ballPos[0] < 1195 and y1< ballPos[1] < y1+h1:
+                    speedX = -speedX
+                    ballPos[0] -= 30
+
+    # Game Over
+    if ballPos[0]<40 or ballPos[0] > 1195:
+        gameOver = True
+
+    if gameOver:
+        img = imgGameOver
+
+    # if game not over move the ball
+    else:
+
+        # Move the Ball
+        if ballPos[1] >= 500 or ballPos[1] <= 10:
+            speedY = -speedY
 
 
+        ballPos[0] += speedX
+        ballPos[1] += speedY
 
 
-    # Draw the ball
-    img = cvzone.overlayPNG(img, imgBall, (100,100))
+        # Draw the ball
+        img = cvzone.overlayPNG(img, imgBall, ballPos)
 
 
     cv2.imshow("Image", img)
