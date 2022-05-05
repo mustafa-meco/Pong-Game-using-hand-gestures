3ba5a10d8ac139b6cd556c9f211add32d2a39a54
Mustafa <musgamal05@gmail.com>
2022-05-05 20:56:46 +0200

    Added Multiplayer mode

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 
+def displayGif(gif, wind = "Image"):
+    i = 20
+    while True:
+        ret, img = gif.read()
+        if ret:
+            cv2.imshow(wind, img)
+        else:
+            break
+        cv2.waitKey(i)
+        i -= 1
+
+
+
+
+
+
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
@@ -11,9 +28,14 @@ cap.set(4, 720)
 # Importing all images
 imgBackground = cv2.imread('Resources/Background.png')
 imgGameOver = cv2.imread('Resources/gameOver.png')
+imgGameStart = cv2.imread('Resources/gameStart.png')
 imgBall = cv2.imread('Resources/ball.png', cv2.IMREAD_UNCHANGED)
 imgBat1 = cv2.imread('Resources/bat1.png', cv2.IMREAD_UNCHANGED)
 imgBat2 = cv2.imread('Resources/bat2.png', cv2.IMREAD_UNCHANGED)
+capSinglePlayer = cv2.VideoCapture('Resources/singlePlayer.gif')
+capTwoPlayers = cv2.VideoCapture('Resources/Two Players.gif')
+capSinglePlayerHover = cv2.VideoCapture('Resources/SinglePlayerHover.gif')
+capTwoPlayersHover = cv2.VideoCapture('Resources/TwoPlayersHover.gif')
 
 # Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=2)
@@ -25,11 +47,16 @@ speedY = 15
 gameOver = False
 score = [0, 0]
 
+gameStart=True
+solo = True
+
 while True:
     _, img = cap.read()
 
     img = cv2.flip(img, 1)
     imgRaw = img.copy()
+
+
 
     # Find the hand and its landmarks
     hands, img = detector.findHands(img, flipType=False)  # with draw
@@ -48,29 +75,63 @@ while True:
             y1 = y - h1/2
             y1 = np.clip(y1, 20, 415)
 
-            if hand['type'] == "Left":
-                img = cvzone.overlayPNG(img, imgBat1, (59,int(y1)))
-                if 59 < ballPos[0] < 59 +w1 and y1< ballPos[1] < y1+h1:
-                    speedX = -speedX
-                    ballPos[0] += 30
-                    score[0] += 1
+            if solo:
+                if hand['type'] == "Left":
+                    img = cvzone.overlayPNG(img, imgBat1, (59,int(y1)))
+                    if 59 < ballPos[0] < 59 +w1 and y1< ballPos[1] < y1+h1:
+                        speedX = -speedX
+                        ballPos[0] += 30
+                        score[0] += 1
 
-            if hand['type'] == "Right":
-                img = cvzone.overlayPNG(img, imgBat2, (1195, int(y1)))
-                if 1195-50 < ballPos[0] < 1195 and y1< ballPos[1] < y1+h1:
-                    speedX = -speedX
-                    ballPos[0] -= 30
-                    score[1] += 1
+                if hand['type'] == "Right":
+                    img = cvzone.overlayPNG(img, imgBat2, (1195, int(y1)))
+                    if 1195-50 < ballPos[0] < 1195 and y1< ballPos[1] < y1+h1:
+                        speedX = -speedX
+                        ballPos[0] -= 30
+                        score[1] += 1
+            else:
+                if hand['type'] == "Right":
+                    img = cvzone.overlayPNG(img, imgBat1, (59, int(y1)))
+                    if 59 < ballPos[0] < 59 + w1 and y1 < ballPos[1] < y1 + h1:
+                        speedX = -speedX
+                        ballPos[0] += 30
+
+                if hand['type'] == "Left":
+                    img = cvzone.overlayPNG(img, imgBat2, (1195, int(y1)))
+                    if 1195 - 50 < ballPos[0] < 1195 and y1 < ballPos[1] < y1 + h1:
+                        speedX = -speedX
+                        ballPos[0] -= 30
+
+
 
     # Game Over
-    if ballPos[0]<40 or ballPos[0] > 1200:
-        gameOver = True
+    if solo:
+        if ballPos[0]<40 or ballPos[0] > 1200:
+            gameOver = True
+    else:
+        if ballPos[0] < 40:
+            score[1] += 1
+        elif ballPos[0] > 1200:
+            score[0] += 1
+        if ballPos[0] < 40 or ballPos[0] > 1200:
+            ballPos = [100, 100]
+            speedX = 15
+            speedY = 15
+        if max(score) == 5:
+            gameOver = True
 
-    if gameOver:
+
+    if gameOver and solo:
         img = imgGameOver
         cv2.putText(img, str(score[0]+score[1]).zfill(2), (585,360), cv2.FONT_HERSHEY_COMPLEX,
                     2.5, (200,0,200), 5)
+    elif gameOver:
+        img = imgGameOver
+        cv2.putText(img, str(score[0]) + ":" + str(score[1]), (585, 360), cv2.FONT_HERSHEY_COMPLEX,
+                    2.5, (200, 0, 200), 5)
 
+    elif gameStart:
+        img = imgGameStart
 
     # if game not over move the ball
     else:
@@ -93,14 +154,30 @@ while True:
     img[580:700, 20:233] = cv2.resize(imgRaw, (213,120))
 
     cv2.imshow("Image", img)
+
     key = cv2.waitKey(1)
     if key == ord('r'):
         ballPos = [100, 100]
-        speedX = 10
-        speedY = 10
+        speedX = 15
+        speedY = 15
         gameOver = False
         score = [0, 0]
         imgGameOver = cv2.imread('Resources/gameOver.png')
+        gameStart = True
+    elif key == ord('1') and gameStart :
+        solo = True
+        gameStart = False
+        displayGif(capSinglePlayer)
+    elif key == ord('2') and gameStart:
+        solo = False
+        gameStart = False
+        displayGif(capTwoPlayers)
+    elif key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
 
 
 
